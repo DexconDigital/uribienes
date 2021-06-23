db94d3d2c4ac9e21fb2a016068b9f6860c5c64c4
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-06-23 10:15:05 -0500

    Ajustes a pdo

--- notices/bd_function.php
+++ notices/bd_function.php
@@ -1,9 +1,18 @@
 <?php
 
-function Conect()
-{
-   $echo = mysqli_connect("localhost","biinyuco_afizus","Afiz2018*","biinyuco_afiz");
-    return $echo;
+function Conect() {
+    // $echo = mysqli_connect("localhost","biinyuco_afizus","Afiz2018*","biinyuco_afiz");
+    // return $echo;
+    try {
+        $echo2 = new PDO( 'mysql:host=localhost;dbname=dexcondigital_noticias_inmueble', 'root', '' );
+        $echo2->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $echo2->exec("set names utf8");
+        return $echo2;
+
+    } catch ( Exception $e ) {
+
+        die( 'Error:' . $e->getMessage() );
+    }
 }
 
 ?>
--- notices/noticesJSON.php
+++ notices/noticesJSON.php
@@ -1,27 +1,32 @@
 <?php
+header( 'Access-Control-Allow-Origin: *' );
+require_once( "bd_function.php" );
 
-require_once("bd_function.php");
-
-$link = Conect();
+$con = Conect();
 $array = array();
 
-    $sql = "SELECT * FROM noticias";
-    $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-    while ($field = mysqli_fetch_array($result)) {
-        $nombre = $field['nombre'];
-        $id = $field['id'];
-        $descripcion = $field['descripcion'];
-        $imagen = $field['imagen'];
-        $noticia = $field['noticia'];
-        $fecha = $field['fecha'];
-        $array[] = array(
-            'titulo' => $nombre,
-            'id' => $id,
-            'descripcion' => $descripcion,
-            'imagen' => $imagen,
-            'noticia' => $noticia,
-            'fecha' => $fecha
-        );
-    }
-$json_string = json_encode($array, JSON_UNESCAPED_UNICODE);
+$sql = "SELECT * FROM noticias where id_inmobiliaria2 = 19 order by id DESC";
+$result = $con->prepare( $sql );
+$result->execute();
+$resultado = $result->fetchAll();
+
+foreach ( $resultado as $key => $field ) {
+    $nombre = $field['nombre'];
+    $id = $field['id'];
+    $descripcion = $field['descripcion'];
+    $imagen = $field['imagen'];
+    $noticia = $field['noticia'];
+    $fecha = $field['fecha'];
+    $array[] = array(
+        'titulo' => $nombre,
+        'id' => $id,
+        'descripcion' => $descripcion,
+        'imagen' => $imagen,
+        'noticia' => $noticia,
+        'fecha' => $fecha
+    );
+}
+
+
+$json_string = json_encode( $array, JSON_UNESCAPED_UNICODE );
 echo $json_string;
